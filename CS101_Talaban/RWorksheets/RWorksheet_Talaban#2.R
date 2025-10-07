1f0764c0a3211b8d2bcdf136aa512e1ee5942efd
lysylbess <lysylbess.talaban@students.isatu.edu.ph>
2025-10-07 05:13:04 +0000

    Added Files

--- CS101_Talaban/RWorksheets/RWorksheet_Talaban#2.R
+++ CS101_Talaban/RWorksheets/RWorksheet_Talaban#2.R
@@ -20,17 +20,131 @@ ages <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
          24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26,
          18) 
 
-#a.
+#a. Access 3rd element, what is the value?
 ages[3]
 #the value is 22
 
-#b.
+#b. Access 2nd and 4th element, what are the values?
 ages[c(2,4)]
-#the values are 28 and 26
+#the values are 28 and 36
 
-#c.
-ages[-1]
-#all values except 34
+#c. Access all but the 1st element is not included. Write the R code and its output.
+age[c(2:50)]
+# OUTPUT
+#> # 3b.
+#  > age[c(2, 4)]
+#[1] 28 36
+#> # the values are 28 and 36
+#  > 
+#  > # 3c.
+#  > age[c(2:50)]
+#[1] 28 22 36 27 18 52 39 42 29 35 31 27 22 37 34 19 20 57 49 50 37 46 25 17 37 43 53 41 51 35 24 33 41 53 40 18 44 38
+#[39] 41 48 27 39 19 30 61 54 58 26 18
+
+# 4.
+x <- c("first"=3, "second"=0, "third"=9)
+x
+
+# a. Print the results. Then access x[c("first", "third")]. Describe the output.
+x[c("first", "third")]
+# the output is displays a 2x2 table with the corresponding columns and rows
+
+# b. Write the code and its output.
+# OUTPUT
+#x <- c("first"=3, "second"=0, "third"=9)
+#> x
+#first second  third 
+#3      0      9 
+#> 
+#  > # 4a.
+#  > x[c("first", "third")]
+#first third 
+#3     9 
+#> # the output is displays a 2x2 table with the corresponding columns and rows
+#  > 
+#  > # 4b.
+#  > x
+#first second  third 
+#3      0      9
+
+# 5. Create a sequence x from -3:2.
+x <- seq(-3:2)
+
+# a. Modify 2nd element and change it to 0; Describe the output.
+x[2] <- 0
+x
+# 5b. Write the code and its output. 
+# OUTPUT
+#> x[2] <- 0
+#> x
+#[1] 1 0 3 4 5 6
+
+# 6.
+Month <- c("Jan", "Feb", "March", "Apr", "May", "June")
+Price_per_liter_PhP <- c(52.50, 57.25, 60.00, 65.00, 74.25, 54.00)
+Purchase_quantity_Liters <- c(25, 30, 40, 50, 10, 45)
+
+# a. Create a data frame for month, price per liter (php) and purchase-quantity (liter). Write the codes.
+df <- data.frame(
+  Month = Month,
+  Price_per_liter_PhP = Price_per_liter_PhP,
+  Purchase_quantity_Liters = Purchase_quantity_Liters
+)
+
+df
+# b. What is the average fuel expenditure of Mr. Cruz from Jan to June? Note: Use weighted.mean(liter, purchase)
+weighted.mean(Price_per_liter_PhP, Purchase_quantity_Liters)
+
+# 7.
+# a. Type “rivers” in your R console. Create a vector data with 7
+# elements, containing the number of elements (length) in rivers,
+# their sum (sum), mean (mean), median (median), variance (var)
+# standard deviation (sd), minimum (min) and maximum (max).
+data <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
+          sd(rivers), min(rivers), max(rivers))
+data
+# b. What are the results?
+# the result are 141, 83357, 591.1844, 425, 243908, 493, 135,  and 3710 correspondingly
+
+# c. Write the code and its outputs.
+#data <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
+#          +           sd(rivers), min(rivers), max(rivers))
+#> data <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
+#            +           sd(rivers), min(rivers), max(rivers))
+#> data
+#[1]    141.0000  83357.0000    591.1844    425.0000 243908.4086    493.8708    135.0000   3710.0000
+
+# 8.
+# a. Create vectors according to the above table. Write the codes.
+Power_Ranking <- c(1:25)
+
+Celebrity_Name <- c(
+  "Tom Cruise", "Rolling Stones", "Oprah Winfrey", "U2", "Tiger Woods", 
+  "Steven Spielberg", "Howard Stern", "50 Cent", "Cast of the Sopranos", 
+  "Dan Brown", "Bruce Springsteen", "Donald Trump", "Muhammad Ali", 
+  "Paul McCartney", "George Lucas", "Elton John", "David Letterman", 
+  "Phil Mickelson", "J.K Rowling", "Bradd Pitt", "Peter Jackson", 
+  "Dr. Phil McGraw", "Jay Lenon", "Celine Dion", "Kobe Bryant"
+)
+
+Pay_Millions <- c(
+  67, 90, 225, 110, 90, 332, 302, 41, 52, 88, 55, 44, 55, 
+  40, 233, 34, 40, 47, 75, 25, 39, 45, 32, 40, 31
+)
+
+# b. Modify the power ranking and pay of J.K. Rowling. Change power ranking to 15 and pay to 90. Write the codes and its output.
+Power_Ranking[19] <- 15
+Pay_Millions[19] <- 90
+
+
+# c. Interpret the data.
+
+Modified_Data <- data.frame(
+  Ranking = Power_Ranking, 
+  Celebrity = Celebrity_Name, 
+  Pay = Pay_Millions)
+
+Modified_Data
 
 
 
